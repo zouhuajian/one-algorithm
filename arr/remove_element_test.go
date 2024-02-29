c61732e2e2139faef9f18b76cb9d836632b5c436
Jay <zouhuajianclever@gmail.com>
2024-02-29 21:11:40 +0800

    arr

---
+++ arr/remove_element_test.go
@@ -0,0 +1,10 @@
+package arr
+
+import (
+	"testing"
+)
+
+func TestRemoveElement(t *testing.T) {
+	l := removeElement([]int{1}, 1)
+	println(l)
+}
---
+++ arr/rotate.go
@@ -0,0 +1,7 @@
+package arr
+
+// https://leetcode.cn/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150
+// 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+func rotate(nums []int, k int) {
+
+}
